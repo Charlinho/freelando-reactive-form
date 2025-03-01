bbf0615ad06e03a87eaab74c9e0eb93982819a84
Charleston Campos <charlinhocjc@gmail.com>
2025-03-01 06:49:15 -0300

    Vídeo 1.4 – Gerenciador de Etapas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+
 import { RouterOutlet } from '@angular/router';
-import { CadastroFormComponent } from './pages/cadastro-form/cadastro-form.component';
+
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { CommonModule } from '@angular/common';
+
 
 const COMPONENTS = [
   HeaderComponent,
-  FooterComponent,
-  CadastroFormComponent
+  FooterComponent
 ];
 
 const MODULES = [
+  RouterOutlet,
   ReactiveFormsModule
 ];
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CadastroFormComponent } from './pages/cadastro-form/cadastro-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/cadastro/area-atuacao', pathMatch: 'full' },
+  { path: 'cadastro/area-atuacao', component: CadastroFormComponent }
+];
