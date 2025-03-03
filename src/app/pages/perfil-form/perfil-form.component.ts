8cb7c30aeeea157c52b4b88f3a664ad246c784d7
Charleston Campos <charlinhocjc@gmail.com>
2025-03-03 12:34:23 -0300

    Vídeo 3.5 – Campo idiomas & FormArray

--- src/app/pages/perfil-form/perfil-form.component.ts
+++ src/app/pages/perfil-form/perfil-form.component.ts
@@ -2,12 +2,13 @@ import { CadastroService } from './../../shared/services/cadastro.service';
 
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { Habilidade } from '../../shared/models/habilidade.interface';
 import { ChipComponent } from '../../shared/components/chip/chip.component';
 import { Router } from '@angular/router';
+import { Idioma } from '../../shared/models/idioma.interface';
 
 
 @Component({
@@ -89,6 +90,29 @@ export class PerfilFormComponent implements OnInit {
     this.perfilForm.patchValue({ habilidadesSelecionadas });
   }
 
+
+  get idiomasArray(): FormArray {
+    return this.perfilForm.get('idiomas') as FormArray;
+  }
+
+  adicionarIdioma(nome: string = '', nivel: string = ''): void {
+    const idiomaForm = this.fb.group({
+      nome: [nome, Validators.required],
+      nivel: [nivel, Validators.required]
+    });
+
+    this.idiomasArray.push(idiomaForm);
+  }
+
+  removerIdioma(index: number): void {
+    if (index === 0 && this.idiomasArray.at(0).get('nome')?.value === 'Português') {
+      return;
+    }
+
+    this.idiomasArray.removeAt(index);
+  }
+
+
   private inicializarFormulario(): void {
     this.perfilForm = this.fb.group({
       foto: [''],
@@ -97,6 +121,17 @@ export class PerfilFormComponent implements OnInit {
       idiomas: this.fb.array([]),
       portfolio: [''],
       linkedin: ['']
+    });
+
+    this.adicionarIdioma('Português', 'Nativo');
+  }
+
+  private extrairIdiomas(): Idioma[] {
+    return this.idiomasArray.controls.map(control => {
+      return {
+        nome: control.get('nome')?.value,
+        nivel: control.get('nivel')?.value
+      };
     });
   }
 
@@ -107,7 +142,7 @@ export class PerfilFormComponent implements OnInit {
       foto: this.fotoPreview,
       resumo: formValue.resumo,
       habilidadesSelecionadas: formValue.habilidadesSelecionadas,
-      idiomas: [],
+      idiomas: this.extrairIdiomas(),
       portfolio: formValue.portfolio,
       linkedin: formValue.linkedin
     });
