4c537745c75c9a90e186f1d88c978ea2a15d5a26
Charleston Campos <charlinhocjc@gmail.com>
2025-03-04 06:27:55 -0300

    Vídeo 4.1 – Validação Assíncrona no Angular

---
+++ src/app/shared/validators/emailExistente.validator.ts
@@ -0,0 +1,12 @@
+import { AbstractControl, AsyncValidatorFn, ValidationErrors } from "@angular/forms";
+import { Observable, of } from "rxjs";
+
+export function emailExistenteValidator(): AsyncValidatorFn {
+  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+    if (!control.value) {
+      return of(null);
+    }
+
+    return of(null);
+  };
+}
